c7ecb4e863c6f684b9a876ced624bf18ad7d43be
Sch1561-it-043 <Sch1561-it-043@1561.ru>
2023-02-15 15:09:26 +0300

    .

--- easy_list_f1.cpp
+++ easy_list_f1.cpp
@@ -40,14 +40,8 @@ bool contains(int num, int digit) {
 }
 
 bool itc_same_parts_list(const vector<int> &mass) {
-    for (int digit = 0; digit <= 9; digit++) {
-        int count = 0;
-        for (int item: mass) {
-            if (contains(item, digit))
-                count++;
-            if (count >= 2)
-                return true;
-        }
-    }
+    for (int i = 1; i < mass.size(); ++i)
+        if ((mass[i] >= 0 && mass[i - 1] >= 0) || (mass[i] < 0 && mass[i - 1] < 0))
+            return true;
     return false;
 }
